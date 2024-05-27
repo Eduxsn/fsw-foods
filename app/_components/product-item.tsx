80abbf1c6af090e52e02afb62339d9bec15771c1
Eduardo Henrique <eduneves95@gmail.com>
2024-05-27 11:41:50 -0300

    feat: add link to products item component

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Badge } from "./ui/badge";
 import { calculateProductsTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDown } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -18,41 +19,43 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="w-[150px] min-w-[150px] space-y-2">
-      <div className="relative h-[150px] w-full">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          fill
-          className="rounded-lg object-cover shadow-md"
-        />
+    <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
+      <div className="w-full space-y-2">
+        <div className="relative h-[150px] w-full">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            fill
+            className="rounded-lg object-cover shadow-md"
+          />
 
-        {product.discountPercentage && (
-          <Badge className="absolute left-2 top-2 text-xs hover:bg-primary">
-            <ArrowDown size={12} />
-            {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-
-      <div>
-        <h2 className="truncate text-sm">{product.name}</h2>
-        <div className="flex items-center gap-1">
-          <h3 className="font-semibold">
-            {formatCurrency(calculateProductsTotalPrice(product))}
-          </h3>
-          {product.discountPercentage > 0 && (
-            <span className="text-xs text-muted-foreground line-through">
-              {formatCurrency(Number(product.price))}
-            </span>
+          {product.discountPercentage && (
+            <Badge className="absolute left-2 top-2 text-xs hover:bg-primary">
+              <ArrowDown size={12} />
+              {product.discountPercentage}%
+            </Badge>
           )}
         </div>
 
-        <span className="block text-xs text-muted-foreground">
-          {product.restaurant.name}
-        </span>
+        <div>
+          <h2 className="truncate text-sm">{product.name}</h2>
+          <div className="flex items-center gap-1">
+            <h3 className="font-semibold">
+              {formatCurrency(calculateProductsTotalPrice(product))}
+            </h3>
+            {product.discountPercentage > 0 && (
+              <span className="text-xs text-muted-foreground line-through">
+                {formatCurrency(Number(product.price))}
+              </span>
+            )}
+          </div>
+
+          <span className="block text-xs text-muted-foreground">
+            {product.restaurant.name}
+          </span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
