5b8b0d85c0be296dd6c5c4c30da2d890ac40baa2
Eduardo Henrique <eduneves95@gmail.com>
2024-05-28 11:20:34 -0300

    feat: add link to home on header logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,21 +1,29 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-5 pt-6">
-      <div className="relative h-[30px] w-[100px]">
-        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
+    <Link href={"/"}>
+      <div className="flex justify-between px-5 pt-6">
+        <div className="relative h-[30px] w-[100px]">
+          <Image
+            src="/logo.png"
+            alt="FSW Foods"
+            fill
+            className="object-cover"
+          />
+        </div>
+        <Button
+          size={"icon"}
+          variant={"secondary"}
+          className="border-none bg-transparent"
+        >
+          <MenuIcon />
+        </Button>
       </div>
-      <Button
-        size={"icon"}
-        variant={"secondary"}
-        className="border-none bg-transparent"
-      >
-        <MenuIcon />
-      </Button>
-    </div>
+    </Link>
   );
 };
 
