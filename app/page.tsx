87c010e90871a9be4059a1bc2fe7f4ad3a03b64b
Eduardo Henrique <eduneves95@gmail.com>
2024-05-28 11:14:09 -0300

    feat: add recommended product page

--- app/page.tsx
+++ app/page.tsx
@@ -47,10 +47,12 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Button variant="link" className="h-fit p-0 text-primary">
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href={"/products/recommended"}>
+            <Button variant="link" className="h-fit p-0 text-primary">
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <ProductList products={products} />
       </div>
